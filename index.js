f7c376c58313504be036e8ef71fa343e6e1f1df3
Kobby <ebenezertio0706@gmail.com>
2024-11-08 10:43:40 +0000

    Updates

---
+++ index.js
@@ -0,0 +1,13 @@
+import Add from '/svg-icons/Add.svg';
+import Menu from '/svg-icons/Menu.svg';
+import Close from '/svg-icons/Close.svg';
+import Share from '/svg-icons/Share Icon.svg';
+import logo from './logo.png'
+
+export {
+    Add,
+    Menu,
+    Close,
+    Share,
+    logo
+}
